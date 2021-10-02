c25c9ab1f3a77e07d6a4d152d6290d661e7b4f70
Vero <a01275996@itesm.mx>
2021-10-01 20:52:04 -0500

    toolbar y fragments

--- app/src/main/java/mx/tec/bamx/DonativoRegistrado.kt
+++ app/src/main/java/mx/tec/bamx/DonativoRegistrado.kt
@@ -1,31 +1,19 @@
 package mx.tec.bamx
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.operador_donativoregistrado.*
-import kotlinx.android.synthetic.main.operador_registrardonativo.*
+import android.view.ViewGroup
 
-class DonativoRegistrado : AppCompatActivity(), View.OnClickListener {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.operador_donativoregistrado)
-
-        btnEnviar.setOnClickListener(this@DonativoRegistrado)
-        btnEditar.setOnClickListener(this@DonativoRegistrado)
+class DonativoRegistrado : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donativo_registrado, container, false)
     }
 
-    override fun onClick(p0: View?) {
-        when (p0!!.id) {
-            R.id.btnEnviar -> {
-                // Origen, Destino
-                val intent = Intent(this@DonativoRegistrado, DonativoEspontaneo::class.java)
-            }
-            R.id.btnEditar -> {
-                // Origen, Destino
-                val intent = Intent(this@DonativoRegistrado, RegistrarDonativo::class.java)
-            }
-        }
-    }
+
 }
