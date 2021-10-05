012e47f582cb59c2933122d3beffca7053b55f42
Vero <a01275996@itesm.mx>
2021-10-04 19:49:25 -0500

    correccion: de fragment a layout

--- app/src/main/java/mx/tec/bamx/LogIn.kt
+++ app/src/main/java/mx/tec/bamx/LogIn.kt
@@ -14,9 +14,9 @@ class LogIn : AppCompatActivity() {
             println("Diste click en el boton LogIn")
             // Origen, Destino
             //val intent = Intent(this@MainActivity, RegistrarDonativo::class.java)
-            val intent = Intent(this, MainActivity::class.java)
-            // Quita login del stack y deja al MAin como principal
-            intent.flags =Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val intent = Intent(this, RegistrarDonativo::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
     }
